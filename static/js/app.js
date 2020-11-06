f87d35f306cf381f15b3e7c132d9ab359ea31077
Roshini <roshini.12j@gmail.com>
2020-11-06 00:10:12 -0500

    Modified code as my array was rewriting data everytime

--- static/js/app.js
+++ static/js/app.js
@@ -11,69 +11,69 @@ var tbody = d3.select("#tablebody");
 function initialdata()
 {
 
-//Selecting and appending the header 
-let tr = d3.select("thead > tr");
-Object.entries(data[0]).forEach( function([key,value]) {
-    let cell=tr.append("th");
-    cell.text(key);
- }); 
-//Appending data to each row 
-data.forEach( day => {
-    let tr =  tbody.append("tr");
-    Object.entries(day).forEach( function([key,value]) {
-        let cell = tr.append("td");
-        var data = cell.text(value);
-       
-    
+    //Selecting and appending the header 
+    let tr = d3.select("thead > tr");
+    Object.entries(data[0]).forEach( function([key,value]) {
+        let cell=tr.append("th");
+        cell.text(key);
     }); 
-    
-});
-//Making the table into a striped table.
-d3.select("table").attr("class","table table-striped");
+    //Appending data to each row 
+    data.forEach( day => {
+        let tr =  tbody.append("tr");
+        Object.entries(day).forEach( function([key,value]) {
+            let cell = tr.append("td");
+            var data = cell.text(value);
+        
+        
+        }); 
+        
+    });
+    //Making the table into a striped table.
+    d3.select("table").attr("class","table table-striped");
 }
 
 function getdata()
 {
- //Prevent the page from refreshing
- d3.event.preventDefault();
- //Select the date input from the form 
- var date = d3.select("#datetime").node().value;
- console.log(date);
- //Select the city input from the form 
- var city = d3.select("#city").node().value;
- city = city.toLowerCase();
- console.log(city);
- //Select the state input from the form 
- var state = d3.select("#state").node().value;
- state = state.toLowerCase();
- console.log(state);
- //Select the country input from the form 
- var country = d3.select("#country").node().value;
- country = country.toLowerCase();
- console.log(country);
- //Select the shape input from the form 
- var shape = d3.select("#shape").node().value;
- shape = shape.toLowerCase();
- console.log(shape);
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
+    //Select the date input from the form 
+    var date = d3.select("#datetime").node().value;
+    console.log(date);
+    //Select the city input from the form 
+    var city = d3.select("#city").node().value;
+    city = city.toLowerCase();
+    console.log(city);
+    //Select the state input from the form 
+    var state = d3.select("#state").node().value;
+    state = state.toLowerCase();
+    console.log(state);
+    //Select the country input from the form 
+    var country = d3.select("#country").node().value;
+    country = country.toLowerCase();
+    console.log(country);
+    //Select the shape input from the form 
+    var shape = d3.select("#shape").node().value;
+    shape = shape.toLowerCase();
+    console.log(shape);
 
- //Clear the inputed valued 
- d3.select("#datetime").node().value="";
- d3.select("#city").node().value="";
- d3.select("#state").node().value="";
- d3.select("#country").node().value="";
- d3.select("#shape").node().value="";
+    //Clear the inputed valued 
+    d3.select("#datetime").node().value="";
+    d3.select("#city").node().value="";
+    d3.select("#state").node().value="";
+    d3.select("#country").node().value="";
+    d3.select("#shape").node().value="";
 
- //Check to see if any one input field has been entered.
- if(date !== "" || city !== "" || state !== "" || country !== "" || shape !== "")
- {
-     //Build the table with filtered table data
-    tablebuild(date,city,state,country,shape);
+    //Check to see if any one input field has been entered.
+    if(date !== "" || city !== "" || state !== "" || country !== "" || shape !== "")
+    {
+        //Build the table with filtered table data
+        tablebuild(date,city,state,country,shape);
 
- }
- else
- {
-     console.log("Please enter valid data");
- }
+    }
+    else
+    {
+        console.log("Please enter valid data");
+    }
 
 };
 function tablebuild(date,city,state,country,shape)
@@ -89,66 +89,67 @@ function tablebuild(date,city,state,country,shape)
     if(date !== "")
     {
       // Filter the data if date present.
-      filterdata = tableData.filter(data => data.date === date);
-
+      var datedata = tableData.filter(data => data.datetime ===  date);
+      console.log(filterdata);
     }
     else
     {
-        filterdata = tableData;
+        datedata = tableData;
     };
     // Check to see if a city was entered
     if(city !== "")
     {
       // Filter the data if city present.
-      filterdata = tableData.filter(data => data.city === city);
-
+      var citydata = datedata.filter(data => data.city === city);
+      console.log(citydata);
     }
     else
     {
-        filterdata = tableData;
+        citydata = datedata;
     };
     // Check to see if a state was entered
     if(state !== "")
     {
       // Filter the data if date present.
-      filterdata = tableData.filter(data => data.state === state);
-
+      var statedata = citydata.filter(data => data.state === state);
+      console.log(statedata)
     }
     else
     {
-        filterdata = tableData;
+        statedata = citydata;
     };
     // Check to see if a country was entered
     if(country !== "")
     {
       // Filter the data if date present.
-      filterdata = tableData.filter(data => data.country === country);
-
+      var countrydata= statedata.filter(data => data.country === country);
+      console.log(filterdata)
     }
     else
     {
-        filterdata = tableData;
+        countrydata = statedata;
     };
     // Check to see if a shape was entered
     if(shape !== "")
     {
       // Filter the data if date present.
-      filterdata = tableData.filter(data => data.shape === shape);
-
+      var shapedata = countrydata.filter(data => data.shape === shape);
+      console.log(shapedata)
     }
     else
     {
-        filterdata = tableData;
-    };
+        shapedata = countrydata;
+    }
     //Check to see the filterdata
-console.log(filterdata);
-filterdata.forEach((tableData) => {
+    console.log(filterdata);
+    shapedata.forEach((table) => {
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
+    Object.entries(table).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
     });
 });
+
 };
 // Add event listener for submit button
 d3.select("#filter-btn").on("click", getdata);
